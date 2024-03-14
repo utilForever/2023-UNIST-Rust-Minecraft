ec1907b65ad3c2a6fafb8d2e9fa66f8c138f0428
Chris Ohk <utilforever@gmail.com>
2024-03-14 21:23:23 +0900

    feat: Update example code for week X

---
+++ 2 - Example/240313 - Rust Basic + Make Minecraft, Week X/src/shapes.rs
@@ -0,0 +1,105 @@
+use crate::chunk_manager::Sides;
+use crate::UVFaces;
+
+#[rustfmt::skip]
+pub unsafe fn write_unit_cube_to_ptr(
+    ptr: *mut f32,
+    position: (f32, f32, f32),
+    (front_uv, back_uv, top_uv, bottom_uv, left_uv, right_uv): UVFaces,
+    [right, left, top, bottom, front, back]: Sides,
+) -> u32 {
+    let (x, y, z) = position;
+
+    let vertex_size = 5;
+    let vertices_per_face = 6;
+    let face_size = vertex_size * vertices_per_face;
+
+    let mut idx = 0;
+    let mut copied_vertices = 0;
+
+    if front {
+        ptr.offset(idx).copy_from_nonoverlapping([
+            0.0 + x, 0.0 + y, 1.0 + z, front_uv.0, front_uv.1,
+            1.0 + x, 0.0 + y, 1.0 + z, front_uv.2, front_uv.1,
+            1.0 + x, 1.0 + y, 1.0 + z, front_uv.2, front_uv.3,
+            1.0 + x, 1.0 + y, 1.0 + z, front_uv.2, front_uv.3,
+            0.0 + x, 1.0 + y, 1.0 + z, front_uv.0, front_uv.3,
+            0.0 + x, 0.0 + y, 1.0 + z, front_uv.0, front_uv.1,
+        ].as_ptr(), face_size);
+
+        idx += face_size as isize;
+        copied_vertices += vertices_per_face;
+    }
+
+    if back {
+        ptr.offset(idx).copy_from_nonoverlapping([
+            1.0 + x, 0.0 + y, 0.0 + z, back_uv.0, back_uv.1,
+            0.0 + x, 0.0 + y, 0.0 + z, back_uv.2, back_uv.1,
+            0.0 + x, 1.0 + y, 0.0 + z, back_uv.2, back_uv.3,
+            0.0 + x, 1.0 + y, 0.0 + z, back_uv.2, back_uv.3,
+            1.0 + x, 1.0 + y, 0.0 + z, back_uv.0, back_uv.3,
+            1.0 + x, 0.0 + y, 0.0 + z, back_uv.0, back_uv.1,
+        ].as_ptr(), face_size);
+
+        idx += face_size as isize;
+        copied_vertices += vertices_per_face;
+    }
+
+    if left {
+        ptr.offset(idx).copy_from_nonoverlapping([
+            0.0 + x, 0.0 + y, 0.0 + z, left_uv.0, left_uv.1,
+            0.0 + x, 0.0 + y, 1.0 + z, left_uv.2, left_uv.1,
+            0.0 + x, 1.0 + y, 1.0 + z, left_uv.2, left_uv.3,
+            0.0 + x, 1.0 + y, 1.0 + z, left_uv.2, left_uv.3,
+            0.0 + x, 1.0 + y, 0.0 + z, left_uv.0, left_uv.3,
+            0.0 + x, 0.0 + y, 0.0 + z, left_uv.0, left_uv.1,
+        ].as_ptr(), face_size);
+
+        idx += face_size as isize;
+        copied_vertices += vertices_per_face;
+    }
+
+    if right {
+        ptr.offset(idx).copy_from_nonoverlapping([
+            1.0 + x, 0.0 + y, 1.0 + z, right_uv.0, right_uv.1,
+            1.0 + x, 0.0 + y, 0.0 + z, right_uv.2, right_uv.1,
+            1.0 + x, 1.0 + y, 0.0 + z, right_uv.2, right_uv.3,
+            1.0 + x, 1.0 + y, 0.0 + z, right_uv.2, right_uv.3,
+            1.0 + x, 1.0 + y, 1.0 + z, right_uv.0, right_uv.3,
+            1.0 + x, 0.0 + y, 1.0 + z, right_uv.0, right_uv.1,
+        ].as_ptr(), face_size);
+
+        idx += face_size as isize;
+        copied_vertices += vertices_per_face;
+    }
+
+    if top {
+        ptr.offset(idx).copy_from_nonoverlapping([
+            0.0 + x, 1.0 + y, 1.0 + z, top_uv.0, top_uv.1,
+            1.0 + x, 1.0 + y, 1.0 + z, top_uv.2, top_uv.1,
+            1.0 + x, 1.0 + y, 0.0 + z, top_uv.2, top_uv.3,
+            1.0 + x, 1.0 + y, 0.0 + z, top_uv.2, top_uv.3,
+            0.0 + x, 1.0 + y, 0.0 + z, top_uv.0, top_uv.3,
+            0.0 + x, 1.0 + y, 1.0 + z, top_uv.0, top_uv.1,
+        ].as_ptr(), face_size);
+
+        idx += face_size as isize;
+        copied_vertices += vertices_per_face;
+    }
+
+    if bottom {
+        ptr.offset(idx).copy_from_nonoverlapping([
+            0.0 + x, 0.0 + y, 0.0 + z, bottom_uv.0, bottom_uv.1,
+            1.0 + x, 0.0 + y, 0.0 + z, bottom_uv.2, bottom_uv.1,
+            1.0 + x, 0.0 + y, 1.0 + z, bottom_uv.2, bottom_uv.3,
+            1.0 + x, 0.0 + y, 1.0 + z, bottom_uv.2, bottom_uv.3,
+            0.0 + x, 0.0 + y, 1.0 + z, bottom_uv.0, bottom_uv.3,
+            0.0 + x, 0.0 + y, 0.0 + z, bottom_uv.0, bottom_uv.1,
+        ].as_ptr(), face_size);
+
+        // idx += face_size as isize;
+        copied_vertices += vertices_per_face;
+    }
+
+    copied_vertices as u32
+}
