f1a47fa56278694197803f94321877ff52c57f46
Chris Ohk <utilforever@gmail.com>
2024-10-04 23:39:14 +0900

    feat: Update example code for week X

---
+++ 2 - Example/241004 - Rust Basic + Make Minecraft, Week X/src/chunk.rs
@@ -0,0 +1,349 @@
+use crate::chunk_manager::{CHUNK_SIZE, CHUNK_VOLUME};
+use crate::gl_call;
+use bit_vec::BitVec;
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
+use rand::random;
+use std::collections::HashSet;
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+pub enum BlockID {
+    Air,
+    Dirt,
+    GrassBlock,
+    Cobblestone,
+    Obsidian,
+    OakLog,
+    OakLeaves,
+    OakPlanks,
+    Glass,
+    Debug,
+    Debug2,
+}
+
+impl BlockID {
+    pub fn is_air(&self) -> bool {
+        self == &BlockID::Air
+    }
+
+    pub fn is_transparent(&self) -> bool {
+        match self {
+            BlockID::Air | BlockID::OakLeaves | BlockID::Glass => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_transparent_no_leaves(&self) -> bool {
+        match self {
+            BlockID::Air | BlockID::Glass => true,
+            _ => false,
+        }
+    }
+}
+
+impl Distribution<BlockID> for Standard {
+    fn sample<R: rand::Rng + ?Sized>(&self, rng: &mut R) -> BlockID {
+        match rng.gen_range(1..4) {
+            // 0 => BlockID::AIR,
+            1 => BlockID::Dirt,
+            2 => BlockID::Cobblestone,
+            3 => BlockID::Obsidian,
+            _ => BlockID::Air,
+        }
+    }
+}
+
+fn create_vao_vbo() -> (u32, u32) {
+    let mut vao = 0;
+    gl_call!(gl::CreateVertexArrays(1, &mut vao));
+
+    // Position
+    gl_call!(gl::EnableVertexArrayAttrib(vao, 0));
+    gl_call!(gl::VertexArrayAttribFormat(
+        vao,
+        0,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        0
+    ));
+    gl_call!(gl::VertexArrayAttribBinding(vao, 0, 0));
+
+    // Texture coords
+    gl_call!(gl::EnableVertexArrayAttrib(vao, 1));
+    gl_call!(gl::VertexArrayAttribFormat(
+        vao,
+        1,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        3 * std::mem::size_of::<f32>() as u32
+    ));
+    gl_call!(gl::VertexArrayAttribBinding(vao, 1, 0));
+
+    // Normals
+    gl_call!(gl::EnableVertexArrayAttrib(vao, 2));
+    gl_call!(gl::VertexArrayAttribFormat(
+        vao,
+        2,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        6 * std::mem::size_of::<f32>() as u32
+    ));
+    gl_call!(gl::VertexArrayAttribBinding(vao, 2, 0));
+
+    // Ambient Occlusion
+    gl_call!(gl::EnableVertexArrayAttrib(vao, 3));
+    gl_call!(gl::VertexArrayAttribFormat(
+        vao,
+        3,
+        1,
+        gl::FLOAT,
+        gl::FALSE,
+        9 * std::mem::size_of::<f32>() as u32
+    ));
+    gl_call!(gl::VertexArrayAttribBinding(vao, 3, 0));
+
+    let mut vbo = 0;
+    gl_call!(gl::CreateBuffers(1, &mut vbo));
+
+    gl_call!(gl::VertexArrayVertexBuffer(
+        vao,
+        0,
+        vbo,
+        0,
+        (10 * std::mem::size_of::<f32>()) as i32
+    ));
+
+    (vao, vbo)
+}
+
+pub struct ChunkColumn {
+    pub chunks: [Chunk; 16],
+}
+
+impl ChunkColumn {
+    pub fn new() -> Self {
+        Self {
+            chunks: [
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+                Chunk::empty(),
+            ],
+        }
+    }
+
+    pub fn random() -> Self {
+        Self {
+            chunks: [
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+                Chunk::random(),
+            ],
+        }
+    }
+
+    pub fn full_of_block() -> Self {
+        Self {
+            chunks: [
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+                Chunk::full_of_block(BlockID::Cobblestone),
+                Chunk::full_of_block(BlockID::Dirt),
+            ],
+        }
+    }
+}
+
+pub struct Chunk {
+    blocks: [BlockID; CHUNK_VOLUME as usize],
+    visible_block_faces: BitVec,
+
+    pub vao: u32,
+    pub vbo: u32,
+    pub vertices_drawn: u32,
+    pub dirty: bool,
+    pub dirty_neighbours: HashSet<(i32, i32, i32)>,
+}
+
+impl Default for Chunk {
+    fn default() -> Self {
+        Self::empty()
+    }
+}
+
+impl Chunk {
+    fn all_neighbours() -> HashSet<(i32, i32, i32)> {
+        let mut hash_set = HashSet::new();
+
+        hash_set.insert((1, 0, 0));
+        hash_set.insert((0, 1, 0));
+        hash_set.insert((0, 0, 1));
+        hash_set.insert((-1, 0, 0));
+        hash_set.insert((0, -1, 0));
+        hash_set.insert((0, 0, -1));
+
+        hash_set
+    }
+
+    pub fn empty() -> Chunk {
+        let (vao, vbo) = create_vao_vbo();
+
+        Chunk {
+            blocks: [BlockID::Air; CHUNK_VOLUME as usize],
+            visible_block_faces: BitVec::from_elem(6 * 16 * 16 * 16, false),
+            vao,
+            vbo,
+            vertices_drawn: 0,
+            dirty: false,
+            dirty_neighbours: Chunk::all_neighbours(),
+        }
+    }
+
+    pub fn full_of_block(block: BlockID) -> Chunk {
+        let (vao, vbo) = create_vao_vbo();
+
+        Chunk {
+            blocks: [block; CHUNK_VOLUME as usize],
+            visible_block_faces: BitVec::from_elem(6 * 16 * 16 * 16, false),
+            vao,
+            vbo,
+            vertices_drawn: 0,
+            dirty: true,
+            dirty_neighbours: Chunk::all_neighbours(),
+        }
+    }
+
+    pub fn random() -> Chunk {
+        let (vao, vbo) = create_vao_vbo();
+
+        let mut chunk = Chunk {
+            blocks: [BlockID::Air; CHUNK_VOLUME as usize],
+            visible_block_faces: BitVec::from_elem(6 * 16 * 16 * 16, false),
+            vao,
+            vbo,
+            vertices_drawn: 0,
+            dirty: true,
+            dirty_neighbours: Chunk::all_neighbours(),
+        };
+
+        for i in 0..chunk.blocks.len() {
+            chunk.blocks[i] = random::<BlockID>();
+        }
+
+        chunk
+    }
+
+    #[inline]
+    fn coords_to_index(x: u32, y: u32, z: u32) -> usize {
+        (y * (CHUNK_SIZE * CHUNK_SIZE) + z * CHUNK_SIZE + x) as usize
+    }
+
+    #[inline]
+    pub fn get_block(&self, x: u32, y: u32, z: u32) -> BlockID {
+        self.blocks[Chunk::coords_to_index(x, y, z)]
+    }
+
+    #[inline]
+    pub fn set_block(&mut self, x: u32, y: u32, z: u32, block: BlockID) {
+        self.blocks[Chunk::coords_to_index(x, y, z)] = block;
+        self.dirty = true;
+
+        if x == 0 {
+            self.dirty_neighbours.insert((-1, 0, 0));
+        } else if x == 15 {
+            self.dirty_neighbours.insert((1, 0, 0));
+        }
+
+        if y == 0 {
+            self.dirty_neighbours.insert((0, -1, 0));
+        } else if y == 15 {
+            self.dirty_neighbours.insert((0, 1, 0));
+        }
+
+        if z == 0 {
+            self.dirty_neighbours.insert((0, 0, -1));
+        } else if z == 15 {
+            self.dirty_neighbours.insert((0, 0, 1));
+        }
+    }
+}
+
+// Iterator that iterates overall possible block coordinates of a chunk on all 3 axis
+// Equivalent in functionality to a triple for loop from 0 to 15 each
+pub struct BlockIterator {
+    x: u32,
+    y: u32,
+    z: u32,
+}
+
+impl BlockIterator {
+    pub fn new() -> BlockIterator {
+        BlockIterator { x: 0, y: 0, z: 0 }
+    }
+}
+
+impl Iterator for BlockIterator {
+    type Item = (u32, u32, u32);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.y == CHUNK_SIZE {
+            None
+        } else {
+            let ret = (self.x, self.y, self.z);
+
+            self.x += 1;
+
+            if self.x >= CHUNK_SIZE {
+                self.x = 0;
+                self.z += 1;
+
+                if self.z >= CHUNK_SIZE {
+                    self.z = 0;
+                    self.y += 1;
+                }
+            }
+
+            Some(ret)
+        }
+    }
+}
